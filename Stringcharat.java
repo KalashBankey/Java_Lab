5bd67757f5f9cc9f5805b6558d49cc953abec4bf
KalashBankey <143742175+KalashBankey@users.noreply.github.com>
2023-09-13 11:55:34 +0530

    Add files via upload

---
+++ Stringcharat.java
@@ -0,0 +1,9 @@
+//Convert the string from lower case to upper case or visa-versa
+public class Stringcharat
+{
+	public static void main(String[] args) {
+		
+	String s="Hello";
+	System.out.println(s.charAt(3));
+	}
+}
