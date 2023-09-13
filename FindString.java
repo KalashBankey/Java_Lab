8125c288375c4ca9c53878194fc79a76abd0ab3e
KalashBankey <143742175+KalashBankey@users.noreply.github.com>
2023-09-13 12:52:20 +0530

    Update FindString.java

--- FindString.java
+++ FindString.java
@@ -6,8 +6,11 @@ public class FindString
 	    boolean per=false;
 	    for(int i=0;i<a.length();i++)
 	    {
+		if(a.charAt(i)=='e')
+		{
 	        per=true;
 	        break;
+		}
 	    }
 	    System.out.println(per);
 	}
