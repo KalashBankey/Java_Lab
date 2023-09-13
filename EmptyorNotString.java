ff45074acf09027bf45d8a0fdfdc69b9e9cc6bb6
KalashBankey <143742175+KalashBankey@users.noreply.github.com>
2023-09-13 11:49:12 +0530

    Add files via upload

---
+++ EmptyorNotString.java
@@ -0,0 +1,10 @@
+//To check whether the string is empty or not
+public class EmptyorNotString
+{
+	public static void main(String[] args) {
+		
+	String s="Hello";
+	boolean b=s.isEmpty();
+	System.out.println(s.isEmpty());
+	}
+}
