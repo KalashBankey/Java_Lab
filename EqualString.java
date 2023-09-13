1cac3a7c5cad7434c83249fdff11174981e25825
KalashBankey <143742175+KalashBankey@users.noreply.github.com>
2023-09-13 12:47:20 +0530

    Add files via upload

---
+++ EqualString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class EqualString
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter s1");
+	    String s1=sc.nextLine();
+	    System.out.println("Enter s2");
+	    String s2=sc.nextLine();
+	    boolean r=s1.equals(s2);
+	    System.out.println(""+r);
+	        
+	
+	}
+}
