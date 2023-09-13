9871195531344f2e01b32fb8f709e94f6ee782ef
KalashBankey <143742175+KalashBankey@users.noreply.github.com>
2023-09-13 12:49:50 +0530

    Add files via upload

---
+++ FindString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FindString
+{
+	public static void main(String[] args) {
+	    String a="Umbrella";
+	    boolean per=false;
+	    for(int i=0;i<a.length();i++)
+	    {
+	        per=true;
+	        break;
+	    }
+	    System.out.println(per);
+	}
+}
