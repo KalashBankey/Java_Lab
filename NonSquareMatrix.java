70ea33fbfecfb30701e569d41390790848826192
KalashBankey <143742175+KalashBankey@users.noreply.github.com>
2023-10-04 12:32:39 +0530

    Add files via upload

---
+++ NonSquareMatrix.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class NonSquareMatrix
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String [][]a={{"Mr.","Mrs.","Ms."},{"Kumar"}};
+	        System.out.println(a[0][0] + a[1][0]);
+	        System.out.println(a[0][1] + a[1][0]);
+	        System.out.println(a[0][2] + a[1][0]);
+	}
+}
