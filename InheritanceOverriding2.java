b79f60cc6daf117d5c00b3e4c6619ed3db7c2bef
KalashBankey <143742175+KalashBankey@users.noreply.github.com>
2023-10-06 16:30:30 +0530

    Add files via upload

---
+++ InheritanceOverriding2.java
@@ -0,0 +1,23 @@
+//to demonstrate method overriding  and inheritance
+class Animal{
+    public void eat(){
+        System.out.println("method eat in parent class");
+    }
+}
+class Dog extends Animal
+{
+    public void eat(){
+        System.out.println("Method eat in subclass");
+    }
+}
+public class InheritanceOverriding2
+{
+	public static void main(String[] args) {
+		Animal a=new Animal();
+		a.eat();
+		Dog d=new Dog();
+		d.eat();
+		Animal c = new Dog();
+		c.eat();
+	}
+}
