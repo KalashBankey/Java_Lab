e5ba1165c78ab87b888c2e8f2f0c5390ceb1f9cf
KalashBankey <143742175+KalashBankey@users.noreply.github.com>
2023-10-13 15:49:25 +0530

    Add files via upload

---
+++ finalvariableEX1.java
@@ -0,0 +1,15 @@
+
+class A{
+   public final void display(){
+       System.out.println("This is a final method");
+   }
+}
+public class finalvariableEX1 extends A{
+    public final void display(){
+       System.out.println("The final method is overridden");
+   }
+    public static void main (String[] args) {
+        finalvariableEX1 a =new finalvariableEX1();
+        a.display();
+    }
+}
